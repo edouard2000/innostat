6b769926a41ea42eca25c809077f75cd50889495
Edouard KWIZERA <136392472+edouard2000@users.noreply.github.com>
2025-05-01 16:40:52 -0400

    Update Home.js
    remove unnecessary link.

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,8 +11,7 @@ const Home = () => {
       <div className="container mx-auto px-4 -mt-4">
         <HeroSection />
         <ServicesSection />
-        <a class="twitter-timeline" href="https://twitter.com/InnoSTAT_Rw?ref_src=twsrc%5Etfw">Tweets by InnoSTAT_Rw</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-      </div>
+    </div>
     </div>
   );
 };
